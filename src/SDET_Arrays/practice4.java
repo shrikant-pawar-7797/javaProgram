e13512e8a0d82563eaa6e73925aab78fa186c3b2
Shrikant Pawar <shrikant@angularminds.com>
2025-01-20 12:42:36 +0530

    Added some array programs

--- src/SDET_Arrays/practice4.java
+++ src/SDET_Arrays/practice4.java
@@ -8,7 +8,6 @@ public class practice4 {
 
 	public static void main(String[] args) {
 
-		
 	}
 
 }
--- src/SDET_Arrays/searchElemnt.java
+++ src/SDET_Arrays/searchElemnt.java
@@ -1,13 +1,41 @@
 package SDET_Arrays;
 
-// 8.) Search element in an Array
+import java.util.Scanner;
 
+// 8.) Search element in an Array
 
 public class searchElemnt {
 
 	public static void main(String[] args) {
 
-		
+		int a[] = { 2, 4, 65, 76, 87, 9, 8, 75, 32, 0, 81 };
+
+		int count = 0;
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter number you want to find : ");
+
+		int num = scanner.nextInt();
+
+		scanner.close();
+
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == num) {
+
+				count++;
+
+			}
+
+		}
+		if (count == 1) {
+			System.out.println("Your searching number is present");
+		}
+
+		else {
+			System.out.println("Your searching number is not present");
+		}
+
 	}
 
 }
