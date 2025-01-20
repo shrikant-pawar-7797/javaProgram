0e542b1233a449add06dcb42f054dd2473b671de
Shrikant Pawar <shrikant@angularminds.com>
2025-01-20 12:11:19 +0530

    Added practice3 program

--- src/SDET_Arrays/practice3.java
+++ src/SDET_Arrays/practice3.java
@@ -8,7 +8,28 @@ public class practice3 {
 
 	public static void main(String[] args) {
 
-		
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		int evenCount = 0;
+		int oddCount;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] % 2 == 0) {
+
+				evenCount++;
+			}
+
+		}
+
+		oddCount = arr.length - evenCount;
+
+		System.out.println("Total numbers in an Array : " + arr.length);
+
+		System.out.println("Total Even numbers in given array : " + evenCount);
+
+		System.out.println("Total Odd numbers in given array : " + oddCount);
+
 	}
 
 }
