cd1d714aac322e0d53e1ca77293a8dbc11f00f3e
Shrikant Pawar <shrikant@angularminds.com>
2025-01-20 14:22:41 +0530

    New programs added

--- src/SDET_Arrays/smallLarge.java
+++ src/SDET_Arrays/smallLarge.java
@@ -6,7 +6,29 @@ public class smallLarge {
 
 	public static void main(String[] args) {
 
-		
+		int min = 0;
+		int max = 0;
+		int a[] = { 2, 1, 3, 4, 5, 8, 7, 6, 99, 8, 0, 3, 33, 21, 67 };
+
+		for (int i = 0; i < a.length; i++) {
+
+			if (a[i] < min) {
+				min = a[i];
+			}
+		}
+
+		for (int j = 0; j < a.length; j++) {
+
+			if (a[j] > max) {
+
+				max = a[j];
+			}
+
+		}
+
+		System.out.println(min);
+		System.out.println(max);
+
 	}
 
 }
--- src/SDET_Arrays/sortArray.java
+++ src/SDET_Arrays/sortArray.java
@@ -1,11 +1,22 @@
 package SDET_Arrays;
 
+import java.util.Arrays;
+
 // 3.) Sort an array without using in-built method
 
 public class sortArray {
 
 	public static void main(String[] args) {
 
+		int arr[]= {1,2,3,6,5,4,7,87,97,5,433,232,243,656,78};
+		
+		Arrays.sort(arr);
+		
+		//System.out.println("Sorted Array: " + Arrays.toString(arr));
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
 		
 		
 	}
