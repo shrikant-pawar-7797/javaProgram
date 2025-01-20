f21393a5ae32ffcc66f42b1dbc56fb6406e7fd4e
Shrikant Pawar <shrikant@angularminds.com>
2025-01-20 12:03:20 +0530

    Added new programs

--- src/SDET_Arrays/firstAndLastElement.java
+++ src/SDET_Arrays/firstAndLastElement.java
@@ -5,8 +5,17 @@ package SDET_Arrays;
 public class firstAndLastElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 87, 67, 54, 32, 4, 323, 45 };
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if ((i == 0) || (i == arr.length - 1)) {
+
+				System.out.println(arr[i]);
+			}
+
+		}
 
 	}
-
 }
--- src/SDET_Arrays/practice2.java
+++ src/SDET_Arrays/practice2.java
@@ -2,10 +2,32 @@ package SDET_Arrays;
 
 // 10.) Find Minimum and Maximum from an Array
 
-
 public class practice2 {
 
 	public static void main(String[] args) {
+
+		int arr[] = { 14, 5, 6, 7, 32, 54, 75, 87, 23, 43, 98 };
+
+		int max = arr[0];
+		int min = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+
+		System.out.println("Minimum value from an Array : " + min);
+
+		System.out.println("Maximum value from an Array : " + max);
 
 	}
 
